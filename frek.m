e250ffdaf70fb6f00faf93f1e65160c06521c26e
Emin Gençoğlu <muhammet.gencoglu@LAB263.std.izu.edu.tr>
2015-12-22 13:17:08 +0200

    frek.m güncellendi

--- frek.m
+++ frek.m
@@ -1,5 +1,24 @@
-function [frekans]=frek(nota,oktav)%frekansý oktava göre döndürüyor
-notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'};
-n=strcmp(notalar,nota);%nota dizide aratýlýyor
-m=find(n,1);
-frekans=round(16.35*(2^oktav)*(2^((m-1)/12)));%sayýsal olarak frekansý oluþturdu
+function [frekans]=frek(nota,oktav) %Girilen nota ve oktava göre frekans döndürülüyor.
+notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'}; %Notalarýmýz bir dizi içerisinde yazýldý.
+n=strcmp(notalar,nota); %Fonksiyona gönderdiðimiz notayý dizi içerisinde aratýr ve buduðu indekse 1 atar
+m=find(n,1);%n dizisinde 1 deðerinin indeksini döndürür.
+
+if (nargin==0)
+    
+    disp ('Lütfen veri giriþi yapýnýz: ')
+    
+else if (nargin==1)
+        if(m==13)
+            frekans=0;
+        else
+           frekans=floor(16.35*(2^4)*(2^((m-1)/12))); % Frekans fonksiyonu oluþturuldu. 
+           
+        end
+    else 
+        if (m==13) 
+            frekans=0;
+        else frekans=floor(16.35*(2^oktav)*(2^((m-1)/12))); % Frekans fonksiyonu oluþturuldu.
+        end
+        
+    end
+end
