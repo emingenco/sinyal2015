d0048ecf5a60042a9ee170fc1dec50dd0ef7f50a
Emin Gençoğlu <muhammet.gencoglu@LAB263.std.izu.edu.tr>
2016-01-05 12:51:08 +0200

    sentez dosyası guncellendi

--- sentez.m
+++ sentez.m
@@ -1,3 +1,4 @@
+%%
 [nota,b,c]=textread('Notalar.txt','%s %s %s'); %Notalar.txt dosyasi okunarak sutunlar bir degere atandi
 fs=8192;%ornekleme degiskene atandi
 ds=round(fs/100); %durma suresi 
@@ -6,12 +7,15 @@ notalar=zeros(1,fs); %ornekleme suresi kadar 0 dan olusan bir dizi olusturuldu
 a=zeros(1,length(notalar)); %notalar matrisi boyutlarinda 0 dan olusan bir dizi olusturuldu
 durak=zeros(1,ds); %durma suresi boyutunda 0 dan olusan bir dizi olusturuldu
 oktav=str2num(cell2mat(b)); %dosyadan cell olarak okunan degiskenler double degere donderildi
+%%
    for i=1:(length(nota)) %nota dizisi boyu kadar dongu olusturuldu
        vurus((i),1)=str2num(cell2mat(c(i,1))); %dosyadan cell olarak okunan degiskenler double degere donusturuldu
        frekans((i),1)=frek(nota((i),1),oktav((i),1)); %daha onceden olusturulmus frek fonsiyonu donusturuldu
        [x,t]=note(frekans((i),1),vurus((i),1)); %daha onceden olusturulmus note fonksiyonu donusturuldu
        notalar=[notalar durak x]; %note fonsiyonunun dondurdurulen sinyal degeri aralarina daha once olusturulmus durak dizisi de yerle�tirilerek birle�tirildi
    end %for dongusu
+   plot(notalar)
+   %%
  notalarecho=zeros(1,(length(notalar))); %notalar matrisi boyutlarinda 0 dan olusan bir dizi olusturuldu
  for i=1:(length(notalar)) %notalar dizisi boyu kadar dongu olusturuldu
      f=(go+i); %echo olusturmak icin gerekli olan gecikme suresi donguye sokuldu
@@ -21,8 +25,11 @@ oktav=str2num(cell2mat(b)); %dosyadan cell olarak okunan degiskenler double dege
       notalarecho(i)=notalar(i)*0.3+0; 
      end
  end 
+ plot(notalarecho)
+ %%
   normalizenotalarecho=zeros(1,(length(notalar)));%normalizasyon islemi yapilmasi icin degisken tanimi
   normalizenotalarecho=notalarecho/max(abs(notalarecho));%sinyalin tepe degerleri 1'e normalize ediliyor
+  plot(normalizenotalarecho)
 sound(normalizenotalarecho) %sinyal caldiriliyor.
 
     
